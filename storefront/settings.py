9b20cc1820c31803ab1fb7e47daad8fed6f71564
Phase <phasetumurere@gmail.com>
2024-03-25 14:12:02 +0200

    e_commerce-Django

--- storefront/settings.py
+++ storefront/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'Madede_11',
+        'PORT': '3307'
     }
 }
 
