6f9b23a84a8b6a2892d2526fb40d444707585a5b
Phase <phasetumurere@gmail.com>
2024-05-01 13:04:54 +0200

    Serializing Object

--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 
 from rest_framework.decorators import api_view 
@@ -12,7 +12,9 @@ from .serializers import ProductSerializer
 
 @api_view()
 def product_list(request):
-    return Response('All the Products Home Page') #Replaced Response with HttpResponse
+    querryset = Product.objects.all()
+    serializer = ProductSerializer(querryset, many=True)
+    return Response(serializer.data)
 
 
 def orders(request):
@@ -21,10 +23,8 @@ def orders(request):
 
 @api_view()
 def product_details(request,id):
-    try:
-        
-        product = Product.objects.get(pk=id)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data) 
-    except Product.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+            
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data) 
+    
--- storefront/settings.py
+++ storefront/settings.py
@@ -87,23 +87,14 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'storefront',
-#         'HOST': 'localhost',
-#         'USER': 'root',
-#         'PASSWORD': 'Madede_11',
-#         'PORT': '3307'
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'HOST': os.environ.get('DB_HOST'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'Madede_11',
+        'PORT': '3307'
     }
 }
 
