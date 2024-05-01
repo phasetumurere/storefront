00cb4980fe647982b7efdf5b82abbdfb425ba12b
Phase <phasetumurere@gmail.com>
2024-05-01 12:14:24 +0200

    Serializing Objects

--- store/views.py
+++ store/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 from rest_framework.decorators import api_view 
 from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Product
+from .serializers import ProductSerializer
 
 # Create your views here.
 
@@ -16,4 +21,10 @@ def orders(request):
 
 @api_view()
 def product_details(request,id):
-    return Response(f'Product {id} details') 
+    try:
+        
+        product = Product.objects.get(pk=id)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data) 
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
