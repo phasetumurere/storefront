47d2d56428a924348dd53a7eea21bf3a255b8fd3
Phase <phasetumurere@gmail.com>
2024-07-03 12:56:09 +0200

    fetch users/me endpoint (Current user)

--- storefront/settings.py
+++ storefront/settings.py
@@ -158,7 +158,8 @@ AUTH_USER_MODEL = 'core.user'
 
 DJOSER={
     'SERIALIZERS':{
-        'user_create':'core.serializers.UserCreateSerializer'
+        'user_create':'core.serializers.UserCreateSerializer',
+        'current_user':'core.serializers.UserSerializer'
     }  
 }
 
