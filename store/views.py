dc60f62054edebb020d85a706b3440aadf5a7d3d
Phase <phasetumurere@gmail.com>
2024-07-05 14:00:12 +0200

    Getting Current User

--- store/views.py
+++ store/views.py
@@ -6,7 +6,7 @@ from django_filters.rest_framework import DjangoFilterBackend #Perform filters (
 from rest_framework import status
 from rest_framework.filters import SearchFilter, OrderingFilter
 # from rest_framework.pagination import PageNumberPagination
-from rest_framework.decorators import api_view 
+from rest_framework.decorators import api_view, action 
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, UpdateModelMixin #Create a re useable codes
 from rest_framework.response import Response
@@ -261,6 +261,20 @@ class CartItemsViewSet(ModelViewSet):
 class CustomerViewSet(CreateModelMixin, UpdateModelMixin, RetrieveModelMixin, GenericViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    
+    @action(detail=False, methods= ['GET', 'PUT'])
+    def me(self, request):
+        (customer, created) = Customer.objects.get_or_create(user_id=request.user.id)
+        if request.method == 'GET':            
+            serializer = CustomerSerializer(customer)
+            return Response(serializer.data)
+        elif request.method == 'PUT':
+            serializer = CustomerSerializer(customer, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
+    
+    
     
 # Collection Generic Views
 # class CollectionList(ListCreateAPIView):
